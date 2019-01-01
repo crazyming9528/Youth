8880734f3565b34a652e8646658d6b1a77968a52
crazyming <crazyming9528@qq.com>
2019-01-01 23:30:38 +0800

    news list

--- src/app/news/news-interface.ts
+++ src/app/news/news-interface.ts
@@ -1,6 +1,23 @@
 export interface News {
+  author: string;
+  authorName: string;
+  content: string;
+  contentHtml: string;
+  createTime: string;
+  digest: string;
+  discuss: number;
+  firstImg: string;
   id: string;
+  important: number;
+  likeCount: number;
+  modifyTime: string;
+  status: number;
+  title: string;
+  type: string;
+  typeName: string;
+  viewCount: number;
 }
+
 
 
 export interface Res {
--- src/app/news/news.module.ts
+++ src/app/news/news.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HttpClientModule} from '@angular/common/http';
 import {NewsService} from './news.service';
+import { NewsListComponent } from './news-list/news-list.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [NewsListComponent],
   imports: [
     CommonModule,
     HttpClientModule
-  ]
+  ],
+  exports: [
+    NewsListComponent
+  ],
+  providers: [NewsService]
 })
-export class NewsModule { }
+export class NewsModule {
+}
